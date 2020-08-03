368ae5d75152503005cbf4cc79082edc5672585c
yiming <y3jia@eng.ucsd.edu>
2020-08-02 18:03:20 -0700

    update to 1-12

--- C语言程序设计/第一章/1-12.c
+++ C语言程序设计/第一章/1-12.c
@@ -4,6 +4,22 @@
  */
 
 #include <stdio.h>
+#define IN 1
+#define OUT 0
 
-
+main() {
+    int c, state;
+    state = OUT;
+    while ((c = getchar()) != EOF) {
+        if (c == ' ' || c == '\t' || c =='\n') {
+            if (state == IN) {
+                state = OUT;
+                putchar('\n');
+            }
+        } else {
+            state = IN;
+            putchar(c);
+        }
+    }
+}
 
