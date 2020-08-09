9d3023a9c44092698b4d3c71a6af990959ac0bdd
yiming <y3jia@eng.ucsd.edu>
2020-08-08 19:18:12 -0700

    5/1743

---
+++ 力扣/240. 搜索二维矩阵 II.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix.size() == 0 || matrix[0].size() == 0) {
+            return false;
+        }
+        int x = 0, y = matrix[0].size() - 1;
+        while (x < matrix.size() && y >= 0) {
+            if (matrix[x][y] == target) {
+                return true;
+            } else if (matrix[x][y] < target) {
+                x++;
+            } else {
+                y--;
+            }
+        }
+        return false;
+    }
+};
